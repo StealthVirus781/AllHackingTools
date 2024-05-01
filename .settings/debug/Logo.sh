4a765aa2efe7f9bd86def47b83b451d387484438
Stealth Virus <129469835+StealthVirus781@users.noreply.github.com>
2024-05-01 18:06:38 +0800

    Logo.sh

--- .settings/debug/Logo.sh
+++ .settings/debug/Logo.sh
@@ -8,7 +8,7 @@ echo '
  | ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ 
  |
 [+] Version  : AllHackingTools v2.7.9    -  Debug mode: true
-[+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[+] Github   : https://github.com/StealthVirus871  -  nsi⁣ble responsible for any consequences of reading it. 
+[+] Coded By : Stealth Virus (Міша Коржик)  -  All materials are provided for educational purposes only! 
 [+]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[DEBUG]
 '|lolcat -p 3.0 -a -d 2 -s 20.0
