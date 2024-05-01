0f77e834b86def4877eefe4e3c804d31b8771bcf
Stealth Virus <129469835+StealthVirus781@users.noreply.github.com>
2024-05-01 20:31:00 +0800

    Logo.sh

--- src/Logo.sh
+++ src/Logo.sh
@@ -7,6 +7,6 @@ echo '
  |  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗██║██║░╚███║╚██████╔╝░░░░░░░░░██║░░░╚█████╔╝╚█████╔╝███████╗
  |  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v2.7
 [+] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
-[+] Github   : https://github.com/StealthVirus871 -  nsi⁣ble responsible for any consequences of reading it. 
+[+] Github   : https://github.com/StealthVirus781 -  nsi⁣ble responsible for any consequences of reading it. 
 [+] Coded By : Stealth Virus (Міша Коржик)  -  All materials are provided for educational purposes only! 
 [+]———————-——-–————————-—-–———-——————-—–————-————-—–———————-——–—————–-——[+]'|lolcat -p 1.6 -a -d 1 -s 30.0
