5df1bc3275f83f80ef21488ac3c442f6cea33839
Stealth Virus <129469835+StealthVirus781@users.noreply.github.com>
2024-05-01 18:54:27 +0800

    Uninstall.sh

--- Uninstall/Uninstall.sh
+++ Uninstall/Uninstall.sh
@@ -36,7 +36,7 @@ cd termux-style
 cd
 cd 
 rm -rf AutoUpdateMyTools
-git clone https://github.com/mishakorzik/AutoUpdateMyTools
+git clone https://github.com/StealthVirus871/AutoUpdateMyTools
 cd
 cd
 cd .termux
